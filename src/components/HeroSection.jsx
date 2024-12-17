c71370becd3e6a0af816eab77d18da216ecbe60e
Arjun <arjunvaradiyil203@gmail.com>
2024-12-17 17:34:08 +0530

    updated

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './HeroSection.css';
 import profileImage from '../assets/profileImage.png'; // Replace with your image file
+import { FaLinkedin, FaGithub, FaTwitter } from 'react-icons/fa'; // Importing Font Awesome Icons
 
 const HeroSection = () => {
   return (
@@ -11,9 +12,21 @@ const HeroSection = () => {
           I’m <span className="highlight-text">Arjun</span> V
         </h1>
         <p>
-        A passionate and results-driven Computer Science Engineering student with a versatile skill set, eager to apply programming, problem-solving, and analytical skills as a Full Stack Developer Intern at Entri Elevate to enhance the integrity and efficiency of digital systems across various domains.
+          A passionate and results-driven Computer Science Engineering student with a versatile skill set, eager to apply programming, problem-solving, and analytical skills as a Full Stack Developer Intern at Entri Elevate to enhance the integrity and efficiency of digital systems across various domains.
         </p>
-        <button className="cv-button">Download CV</button>
+        <div className="hero-buttons">
+          <div className="social-icons">
+            <a href="https://www.linkedin.com/in/arjunvaradiyill/" target="_blank" rel="noopener noreferrer">
+              <FaLinkedin />
+            </a>
+            <a href="https://github.com/arjunvaradiyill" target="_blank" rel="noopener noreferrer">
+              <FaGithub />
+            </a>
+          </div>
+          <a href="https://drive.google.com/file/d/1Kp_j0dFRBru4-dBVoHp2jYvPaGvVdGxl/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+            <button className="cv-button">Download CV</button>
+          </a>
+        </div>
       </div>
       <div className="hero-image">
         <img src={profileImage} alt="Profile" />
